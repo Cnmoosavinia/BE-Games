f64a099dd368809a22be8e128044152bb77a866d
Cnmoosavinia <cyrusmoosavinia@gmail.com>
2022-11-14 11:48:43 +0000

    initial file setup complete

---
+++ __tests__/app.test.js
@@ -0,0 +1,12 @@
+const request = require("supertest");
+const seed = require("../db/seeds/seed");
+const testData = require("../db/data/test-data");
+const app = require("../app.js");
+const connection = require("../db/connection.js");
+
+beforeEach(() => {
+  return seed(testData);
+});
+afterAll(() => {
+  connection.end();
+});
